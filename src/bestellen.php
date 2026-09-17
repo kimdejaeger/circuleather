8d626cc7002062af1f23588e5a8a5824e476a772
kim <kf.dejaeger@gmail.com>
2026-09-17 11:24:50 +0200

    pagina's weggehaald en toegevoegd

---
+++ src/bestellen.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'session.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bestellen</title>
+    <link rel="stylesheet" href="style_kim.css">
+
+</head>
+<body>
+    <h1>Bestellen</h1>
+</body>
+</html>
